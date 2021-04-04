d46b8f83c36b3b7da78d5a91443baef26a28e38b
Luis0624 <luis.apgo12@gmail.com>
2021-04-03 21:16:18 -0400

    added to the licenses file

---
+++ Develop/licenses.js
@@ -0,0 +1,12 @@
+const apache ='[Apache License](https://www.apache.org/licenses/LICENSE-2.0">)';
+
+const mit = '[MIT License](https://opensource.org/licenses/MIT)';
+
+const gnu ='[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0.en.html)';
+
+const isc = '[ISC License](https://opensource.org/licenses/ISC)';
+
+module.exports.apache = apache;
+module.exports.mit = mit;
+module.exports.isc = isc;
+module.exports.gnu = gnu;
