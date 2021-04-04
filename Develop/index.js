d89adea004ecd271013b50013de4a9b10adabb66
Luis0624 <luis.apgo12@gmail.com>
2021-04-03 23:20:28 -0400

    completed badges.js and switch statments for correct badge display

--- Develop/index.js
+++ Develop/index.js
@@ -4,34 +4,45 @@ const inquirer = require("inquirer");
 const questions = require("./questions.js");
 const licensesContent = require("./licenses.js");
 const badgesSymbol = require("./badges.js")
-let badges;
+let badge;
 
 // TODO: Create an array of questions for user input
 // const questions = [];
 
 // switch statement for using license answer to get the corresponding badge
 inquirer.prompt(questions).then((data) => {
-    const filename = `Sample-README.md`;
+    
     switch (data.license){
         case "Apache":
+            data.license = licensesContent.apache;
+            badge = badgesSymbol.apache;
             break;
         case "ISC":
+            data.license = licensesContent.isc;
+            badge = badgesSymbol.isc;
             break;
         case "MIT":
+            data.license = licensesContent.mit;
+            badge = badgesSymbol.mit;
             break;
         case "GNU GPLv3":
+            data.license = licensesContent.gnu;
+            badge = badgesSymbol.gnu;
             break;
         case "none":
+            data.license = "";
+            badge = "";
         
     }   
-    
+    const filename = "Generated-README.md";
+
     fs.writeFile(filename, generateHTML(data), (err) =>
         err ? console.log(err) : console.log('Success!')
     );
 });
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
 
